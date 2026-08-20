58f346a86e57eb10f088cdbf33fc6eb99efa1c78
azad-727 <sazad3774@gmail.com>
2026-08-20 22:00:09 +0530

    feat: added ProductKeywordStats Model Class with composite primary key and multiple indexs for faster query processing

---
+++ Backend/tverseIQ/src/main/java/com/tverseIQ/backend/model/ProductKeywordStats.java
@@ -0,0 +1,55 @@
+package com.tverseIQ.backend.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name="product_keyword_stats", indexes = {
+        @Index(name="idx_product_orders",columnList="product_id,cumulative_orders DESC"),
+        @Index(name="idx_keyword_orders", columnList ="keyword,cumulative_orders DESC"),
+        @Index(name="idx_confidence",columnList = "confidence_score , attribution_type")
+})
+@Getter
+@Setter
+@IdClass(ProductKeywordStats.class)
+public class ProductKeywordStats {
+
+    @Id
+    @Column(name="product_id", nullable = false)
+    private Long productId;
+
+    @Column(name="keyword",nullable = false,length=255)
+    private String keyword;
+
+    @Column(name="cumulative_orders",nullable = false)
+    private Integer cumulativeOrders=0;
+
+    @Column(name="cumulative_spend",precision = 12,scale=2,nullable = false)
+    private BigDecimal cumulativeSpend=BigDecimal.ZERO;
+
+    @Column(name="cumulative_sales",precision = 12,scale=2,nullable = false)
+    private BigDecimal cumulativeSales=BigDecimal.ZERO;
+
+    @Column(name="cvr",precision = 5,scale=4,nullable = false)
+    private BigDecimal cvr = BigDecimal.ZERO;
+
+    @Column(name="attributionType",nullable = false,length = 20)
+    private String attributionType;
+
+    @Column(name="confidenceScore",nullable = false,precision = 3,scale = 2)
+    private BigDecimal confidenceScore=BigDecimal.ONE;
+
+    @Column(name="timesAppeared",nullable = false)
+    private Integer timesAppeared =0;
+
+    @Column(name="firstConvertedData")
+    private LocalDate firstConvertedData;
+
+    @Column(name="lastConvertedData")
+    private LocalDate lastConvertedDate;
+
+}
